5136d0f3edbf2f9e1e06afaf16a5db5ad768a24d
Jaime Ruiz <ruizbjaime@gmail.com>
2026-03-20 23:52:14 -0500

    refactor: enhance layout responsiveness in card field component

--- resources/views/components/table/card-field.blade.php
+++ resources/views/components/table/card-field.blade.php
@@ -1,8 +1,8 @@
 @props(['label' => ''])
 
-<div class="flex items-center justify-between gap-2 text-sm">
+<div class="flex min-w-0 items-center justify-between gap-2 text-sm">
     @if ($label)
-        <span class="text-zinc-500 dark:text-zinc-400">{{ $label }}</span>
+        <span class="shrink-0 text-zinc-500 dark:text-zinc-400">{{ $label }}</span>
     @endif
-    <span class="text-right text-zinc-800 dark:text-zinc-200">{{ $slot }}</span>
+    <span class="min-w-0 truncate text-right text-zinc-800 dark:text-zinc-200">{{ $slot }}</span>
 </div>
